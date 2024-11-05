5dc2c85ffd35cd680163dec2ba229ff040c47d54
davi campos jaremczuk <147657664+Tibursinho@users.noreply.github.com>
2024-11-05 14:21:50 -0300

    das

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
-function criaCartao (categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 }
