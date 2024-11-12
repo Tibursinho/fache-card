5d89c480c7ab12fd9db5b6ed0b75e50d249658e3
davi campos jaremczuk <147657664+Tibursinho@users.noreply.github.com>
2024-11-12 14:19:16 -0300

    dsa

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,20 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
+
+cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
+`
